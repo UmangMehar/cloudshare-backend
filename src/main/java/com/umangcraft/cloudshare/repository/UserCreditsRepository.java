01e12d5b05df186907ece3db7c7f67cbb727d649
UmangMehar <umangmehar6@gmail.com>
2025-11-19 04:51:06 +0530

    feat: create UserCreditsRepository with clerkId based retrieval

---
+++ src/main/java/com/umangcraft/cloudshare/repository/UserCreditsRepository.java
@@ -0,0 +1,11 @@
+package com.umangcraft.cloudshare.repository;
+
+import com.umangcraft.cloudshare.documents.UserCredits;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserCreditsRepository extends MongoRepository<UserCredits, String> {
+    Optional<UserCredits> findByClerkId(String clerkId);
+}
