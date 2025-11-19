ae37c44245bc82186093aa9c2a849dbcf4ee8eb3
UmangMehar <umangmehar6@gmail.com>
2025-11-19 09:53:30 +0530

    Updated UserCreditsService

--- src/main/java/com/umangcraft/cloudshare/service/UserCreditsService.java
+++ src/main/java/com/umangcraft/cloudshare/service/UserCreditsService.java
@@ -49,4 +49,13 @@ public class UserCreditsService {
         return userCreditsRepository.save(userCredits);
     }
 
+    public UserCredits addCredits(String clerkId, Integer creditsToAdd, String plan) {
+        UserCredits userCredits = userCreditsRepository.findByClerkId(clerkId)
+                .orElseGet(() -> createInitialCredits(clerkId));
+
+        userCredits.setCredits(userCredits.getCredits() + creditsToAdd);
+        userCredits.setPlan(plan);
+        return userCreditsRepository.save(userCredits);
+    }
+
 }
